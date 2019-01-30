3c6feededb60e4f8b90d642cb06fce1a920a097f
AlexKue <A.kuehnle@hotmail.de>
2019-01-30 02:59:54 +0100

    fix wrong month being displayed

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ $(document).ready(function() {
     var DAY = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var CONF;
 
-
     // function definitions
-
     function updateClock(){
         var today = new Date();
         var h = today.getHours();
@@ -21,11 +19,12 @@ $(document).ready(function() {
     function updateDate(){
         var today = new Date();
         var d = today.getDate();
-        var m = today.getMonth();
+        var m = today.getMonth() + 1; // Months are between 0 and 11
         var y = today.getFullYear();
 
         d = checkTime(d);
         m = checkTime(m);
+        console.log(m)
 
         var dn = DAY[today.getDay()]; // getDay returns week day, 0 being Sunday
         $("#date").html(dn + ", " + d + "." + m + "." + y);
@@ -37,8 +36,11 @@ $(document).ready(function() {
     $.getJSON("commands.json", function(json) {
         CONF = json;
         for(let fav of CONF.links){
-            $("#favs").append('<li><span class="favkey">' + fav.key + '</span><span class="favlink">' + fav.url + '</span></l>');
-        }    });
+            $("#favs").append('<li><span class="favkey">'
+                + CONF.command_key + fav.key
+                + '</span><span class="favlink">' + fav.url + '</span></l>');
+        }
+    });
 
     updateClock();
     updateDate();
@@ -47,29 +49,29 @@ $(document).ready(function() {
     $("form").on("submit", function(e){
         e.preventDefault();
         var input = $("#commands").val().trim();
-
-        if(input.indexOf(':') == 0){
+        var command_key = CONF.command_key;
+        if(input.indexOf(command_key) == 0){
             for(let fav of CONF.links){
-                if(fav.key == input) {
+                let command = command_key + fav.key;
+                if(command == input) {
                     window.location.href = fav.url;
                 }
             }
         } else {
             window.location.href = CONF.search_engine + $("#commands").val();
-
         }
     })
 
     $("#commands").keyup(function(e){
-        var input = $(this).val().trim();
+        var input = $(this).val();
 
         if(input.length == 0){ // hide commandlist when bar is emtpy
             $("#favs").slideUp();
         } else {
             $("#favs").slideDown();
         }
-        if(input.indexOf(':') == 0) { //only hide options with leading ':'
-
+        if(input.indexOf(CONF.command_key) == 0) {
+             //only hide options when typing a command
             $(".favkey").each(function(){
                 if($(this).html().indexOf(input) < 0){
                     $(this).parent().fadeOut(200);
